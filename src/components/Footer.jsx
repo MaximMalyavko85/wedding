9ec67b321aac9e05989731f11e58086a34720469
Maksim <maximmalyavko@yandex.ru>
2025-08-19 00:25:23 +0200

    FEAT: minore correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,25 +2,19 @@ import { motion } from "framer-motion";
 import Image2 from '../assets/divider.png';
 
 const Footer = () => (
-  <motion.footer
-    className="section section_footer"
-    initial={{ opacity: 0, y: 30 }}
-    whileInView={{ opacity: 1, y: 0 }}
-    viewport={{ once: true }}
-    transition={{ duration: 0.5 }}
-  >
-    <div className="footer_content">
-      <h2>МЫ БУДЕМ СЧАСТЛИВЫ ВИДЕТЬ ВАС!</h2> <br/>
-      <h5>подтвердите, пожалуйста своё присутствие на нашем празднике любым удобным для Вас способом</h5> <br/>
-      <h5><b>до 3 сентября 2025 года</b></h5> <br/>
-      <h3 className="section_title">Ваши Аня и Максим</h3>
-      <img className="divider" src={Image2}/>
-      <img className="divider" src={Image2}/>
-      <img className="divider" src={Image2}/>
-    </div>
-    <p>© 2025 Maksim</p>
-  </motion.footer>
-  
+
+    <footer  className="section section_footer" >
+      <div className="footer_content">
+        <h2>МЫ БУДЕМ СЧАСТЛИВЫ ВИДЕТЬ ВАС!</h2> <br/>
+        <h5>подтвердите, пожалуйста своё присутствие на нашем празднике любым удобным для Вас способом</h5> <br/>
+        <h5><b>до 3 сентября 2025 года</b></h5> <br/>
+        <h3 className="section_title">Ваши Аня и Максим</h3>
+        <img className="divider" src={Image2}/>
+        <img className="divider" src={Image2}/>
+        <img className="divider" src={Image2}/>
+      </div>
+      <p>© 2025 Maksim</p>
+    </footer>
 );
 
 export default Footer;

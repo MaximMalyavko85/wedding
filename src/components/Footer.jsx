2422546a39184db9c90acd16e2365d34624a01ea
Maksim <maximmalyavko@yandex.ru>
2025-08-19 00:43:26 +0200

    FEAT: minore correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => (
 
     <footer  className="section section_footer" >
       <div className="footer_content">
-        <h2>МЫ БУДЕМ СЧАСТЛИВЫ ВИДЕТЬ ВАС!</h2> <br/>
-        <h5>подтвердите, пожалуйста своё присутствие на нашем празднике любым удобным для Вас способом</h5> <br/>
+        <h2 className="section_title">Мы будем счастливы видеть Вас!</h2> <br/>
+        <h5>подтвердите, пожалуйста, своё присутствие на нашем празднике любым удобным для Вас способом</h5> <br/>
         <h5><b>до 3 сентября 2025 года</b></h5> <br/>
-        <h3 className="section_title">Ваши Аня и Максим</h3>
+        <h3 className="section_subtitle">Аня и Максим</h3>
         <img className="divider" src={Image2}/>
         <img className="divider" src={Image2}/>
         <img className="divider" src={Image2}/>
